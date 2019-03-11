776c9b0a69ca73bb9f7a75228a830fab180830f9
Ao Li <aoli1996@berkeley.edu>
2019-03-11 16:43:57 -0500

    Needs to support scalabity

--- scripts/git_deploy.sh
+++ scripts/git_deploy.sh
@@ -1,6 +1,6 @@
 read -p "Please enter your username:" username
 read -p "Please enter your netid:" netid
-read -sp "Please enter your password:" password
+read -sp "Please enter your password for your netid:" password
 echo ""
 read -sp "Please enter your github password:" git_password
 echo ""
@@ -18,13 +18,13 @@ then
 if sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "test -d ${git_repo}"
 then
 
-sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "cd ${git_repo}; git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "cd ${git_repo}; git checkout . ; git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
 
 echo ""
 
 else
 
-sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "cd /home/${netid}/mp2; git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "cd /home/${netid}/mp2; git checkout . ;git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
 
 echo ""
 
@@ -38,13 +38,13 @@ if sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs4
 
 then
 
-sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "cd ${git_repo}; git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "cd ${git_repo}; git checkout . ;git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
 
 echo ""
 
 else
 
-sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "cd /home/${netid}/mp2; git clone https://github.com/Unous1996/cs425_sp19_mp2.git"
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "cd /home/${netid}/mp2; git checkout , ;git clone https://github.com/Unous1996/cs425_sp19_mp2.git"
 
 echo ""
 
