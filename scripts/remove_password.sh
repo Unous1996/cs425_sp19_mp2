7bb056e3b36d45a540cbfce0b24fc85632a5b764
Ao Li <aoli1996@berkeley.edu>
2019-03-18 13:15:12 -0500

    Mar18-1315

---
+++ scripts/remove_password.sh
@@ -0,0 +1,55 @@
+read -p "Please enter your username:" username
+read -p "Please enter your netid:" netid
+read -sp "Please enter your password for your netid:" password
+echo ""
+read -sp "Please enter your github password:" git_password
+echo ""
+
+git_repo="https://github.com/username/cs425_sp19_mp2"
+
+for i in {1..10};
+do
+
+if ((i < 10))
+then
+
+# sshpass -p ${password} ssh -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "hostname"
+
+if sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "test -d ${git_repo}"
+then
+
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "cd ${git_repo}; git checkout . ;git config --global credential.helper store ;git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
+
+echo ""
+
+else
+
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-0${i}.cs.illinois.edu "cd /home/${netid}/mp2; git checkout . ;git config --global credential.helper store;git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
+
+echo ""
+
+fi
+
+else
+
+# sshpass -p ${password} ssh -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "hostname"
+
+if sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "test -d ${git_repo}"
+
+then
+
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "cd ${git_repo}; git checkout . ;git config --global credential.helper store;git pull https://github.com/Unous1996/cs425_sp19_mp2.git"
+
+echo ""
+
+else
+
+sshpass -p ${password} ssh -t -o "StrictHostKeyChecking no" ${netid}@sp19-cs425-g16-${i}.cs.illinois.edu "cd /home/${netid}/mp2; git checkout .;git config --global credential.helper store;git clone https://github.com/Unous1996/cs425_sp19_mp2.git"
+
+echo ""
+
+fi
+
+fi
+
+done
