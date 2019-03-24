4b8110f8fde534d93cefe9d43bea436bc8a98bcf
Ao Li <aoli1996@berkeley.edu>
2019-03-24 01:26:34 -0500

    1553408794

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,7 @@
+cd ..
+git commit -a -m "$(date +%s)"
+git push origin master
+cd scripts/
 sh git_deploy.sh
 sh buildkill.sh
 pssh -i -h ~/.pssh_hosts_files "cd /home/aol3/mp2/;sh simulate.sh $(date +%s)"
